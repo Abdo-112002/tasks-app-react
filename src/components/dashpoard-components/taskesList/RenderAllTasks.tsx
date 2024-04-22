2b0bae35dde0364507e31a8b39a6b93443f3248b
Abdelrahman-Ibrahem <bdha26045@gmail.com>
2024-04-22 15:31:20 +0200

    add checkbox to each take
    add checkbox to each take to select any task to let you make bulk some action in collections of takes

--- src/components/dashpoard-components/taskesList/RenderAllTasks.tsx
+++ src/components/dashpoard-components/taskesList/RenderAllTasks.tsx
@@ -1,11 +1,20 @@
 import TaskItem from './TaskItem';
 import { Empty, Spin } from 'antd';
 import useRenderTasksHook from '../../../hooks/dash-hooks/useRenderTasksHook';
+import { useState } from 'react';
+import { TaskTypes } from '../../../store/tasksPageStore';
 
 const RenderAllTasks = () => {
     // handel init page load
-    const { loadingData, filteredTasks } = useRenderTasksHook();
+    const { loadingData, filteredTasks, checkedTaskChange, selectedTask, setSelectedTask } = useRenderTasksHook();
 
+    const [prevValue, selectedValue] = useState<TaskTypes[]>(filteredTasks)
+
+    if (filteredTasks !== prevValue) {
+        setSelectedTask([])
+        selectedValue(filteredTasks);
+    }
+    
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {
@@ -14,7 +23,7 @@ const RenderAllTasks = () => {
                     : !filteredTasks.length
                         ? <div className="col-span-full flex items-center justify-center"><Empty /></div>
                         : filteredTasks?.map((task) => {
-                            return <TaskItem key={crypto.randomUUID()} task={task} />
+                            return <TaskItem selectedTask={selectedTask} checkedTaskChange={checkedTaskChange} key={crypto.randomUUID()} task={task} />
                         })
             }
         </div>
--- src/hooks/dash-hooks/useRenderTasksHook.ts
+++ src/hooks/dash-hooks/useRenderTasksHook.ts
@@ -1,11 +1,19 @@
 import { useEffect, useMemo, useState } from 'react';
 import { filterData, tasksData } from '../../store/tasksPageStore';
 import { useAtomValue } from 'jotai';
+import { CheckboxChangeEvent } from 'antd/es/checkbox';
 
 const useRenderTasksHook = () => {
     const [loadingData, setLading] = useState<boolean>(true);
     const allTasks = useAtomValue(tasksData);
     const filterValue = useAtomValue(filterData);
+    const [selectedTask , setSelectedTask] = useState<string[]>([]);
+
+    const checkedTaskChange = (id : string) => (e : CheckboxChangeEvent) => {
+        if(e.target.checked) {
+            setSelectedTask([...selectedTask , id]);
+        }
+    }
 
     const filteredTasks = useMemo(() => {
         return allTasks.filter((task) => task?.name?.includes(filterValue));
@@ -21,7 +29,7 @@ const useRenderTasksHook = () => {
         };
     }, [setLading]);
 
-    return { loadingData, filteredTasks };
+    return { loadingData, filteredTasks , checkedTaskChange , selectedTask , setSelectedTask};
 };
 
 export default useRenderTasksHook;
